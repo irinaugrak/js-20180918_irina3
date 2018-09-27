48b1a4cc6bc83450cfbb2c0e9db8ffb61ad3e780
irina ugrak <irina.ugrak@gmail.com>
2018-09-27 13:44:39 +0300

    1-module/2-task

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -12,11 +12,14 @@ function print (text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid (name) {
-    //моя функция валидности
+    if(!name || name.indexOf(" ") !== -1 || name.length < 4) {
+        return false;
+    }
+    return true;
 }
 
 function sayHello () {
-    let userName = prompt('Введите ваше имя');
+    let userName = prompt('Введите ваше имя', "");
 
     if (isValid(userName)) {
         print('Welcome back, ' + userName + '!');
