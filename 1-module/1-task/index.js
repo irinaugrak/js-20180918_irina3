2a81da30b964ea158e96db358d35052b543ed94b
irina ugrak <irina.ugrak@gmail.com>
2018-09-27 16:09:54 +0300

    1-module/1-task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,24 @@
+'use strict';
+
+let m = +prompt("Введите натуральное число m: ", "");
+let n = +prompt("Введите натуральное число n: ", "");
+
+let correct = true;
+
+if (parseInt(m) !== m || m <= 0) {
+    correct = false;
+    alert("Некорректно введено число m!");
+};
+
+if (parseInt(n) !== n || n <= 0) {
+    correct = false;
+    alert("Некорректно введено число n!");
+};
+
+if(correct) {
+    alert( pow(m, n) );
+}
+
 /**
  * Power
  * @param {number} m base
@@ -5,5 +26,5 @@
  * @returns {number}
  */
 function pow (m, n) {
-
+    return (n === 1) ? m : m*pow(m, n-1);
 }
