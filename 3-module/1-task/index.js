682f14f07b80d778f8f35d072ad4323ad4d58e56
irina ugrak <irina.ugrak@gmail.com>
2018-10-09 16:33:27 +0300

    3-module/1-task

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,50 @@
+'use strict';
+
+let ivan = {
+    "balance": "$1,425.65",
+    "picture": "http://placehold.it/32x32",
+    "age": 21,
+    "name": "Ivan Branch",
+    "gender": "male",
+    "greeting": "Hello, Golden Branch! You have 7 unread messages.",
+    "favoriteFruit": "banana"
+}
+
+let petr = {
+    "balance": "$1,725.65",
+    "picture": "http://placehold.it/32x32",
+    "age": 42,
+    "name": "Petr Branch",
+    "gender": "male",
+    "greeting": "Hello, Golden Branch! You have 7 unread messages.",
+    "favoriteFruit": "banana"
+}
+
+let maria = {
+    "balance": "$1,925.65",
+    "picture": "http://placehold.it/32x32",
+    "age": 35,
+    "name": "Maria Branch",
+    "gender": "female",
+    "greeting": "Hello, Golden Branch! You have 7 unread messages.",
+    "favoriteFruit": "banana"
+}
+
+let elena = {
+    "balance": "$1,625.65",
+    "picture": "http://placehold.it/32x32",
+    "age": 30,
+    "name": "Elena Branch",
+    "gender": "female",
+    "greeting": "Hello, Golden Branch! You have 7 unread messages.",
+    "favoriteFruit": "banana"
+}
+
+let data = [ivan, petr, maria, elena];
+
+showSalary(data, 40);
+
+
 /**
  * showSalary
  * @param {Array} data - данные о пользователях
@@ -5,6 +52,12 @@
  * @returns {string}
  */
 function showSalary(data, age) {
+    let res = data.filter(function(user) {
+        return user.age <= age;
+    });
+    for(let i=0; i<res.length; i++) {
+        console.log(res[i].name + ', ' + res[i].balance);
+    }
 }
 
 
